9460bca4441b49791bdeffd085fa7c810c5ad844
tsuyoshiwada <v23.b.wada@gmail.com>
2017-08-25 14:15:12 +0900

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  extends: 'airbnb',
+  rules: {
+    'react/jsx-filename-extension': 'off',
+  },
+};
