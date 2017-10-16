0a86bce7ef9821697f52e9f46e522bef87dfb23e
Alexey Bondarenko <alexeybondarenko@me.com>
2017-10-17 01:44:27 +0300

    Brake the example

--- example/Button.stories.js
+++ example/Button.stories.js
@@ -6,21 +6,13 @@ import { withScreenshot } from '../src/';
 import Button from './Button';
 
 storiesOf('Button', module)
-  .add('with text',
-    withScreenshot()(() => (
-      <Button>Default</Button>
-    ))
-  )
-  .add('with primary',
-    withScreenshot({
-      viewport: {
-        width: 400,
-        height: 250,
-      },
-    })(() => (
-      <Button primary>Primary</Button>
-    ))
-  )
+  .addDecorator(withScreenshot())
+  .add('with text', () => (
+    <Button>Default</Button>
+  ))
+  .add('with primary', () => (
+    <Button primary>Primary</Button>
+  ))
   .add('with not capture button', () => (
     <Button>Not Capture</Button>
   ));
