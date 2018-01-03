4df7d29f7772ac6a04d2915866f69875dfb3dc4e
y-kurami <yosuke.kurami@gmail.com>
2018-01-03 14:45:14 +0900

    use imagesLoaded

--- src/angular/with-screenshot.js
+++ src/angular/with-screenshot.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import addons from '@storybook/addons';
+import imagesLoaded from 'imagesloaded';
 import { mergeScreenshotOptions } from '../screenshot-options';
 import { EventTypes } from '../constants';
 
@@ -34,8 +35,16 @@ export default function withScreenshot(options = {}) {
       if (!this.__getStoryContext__) return;
       const delay = { opt };
       emit(EventTypes.COMPONENT_MOUNT, this.__getStoryContext__());
-      // TODO wait for images loaded
-      setTimeout(() => emit(EventTypes.COMPONENT_READY, this.__getStoryContext__()), delay);
+      // FIXME
+      // The "[ng-version]" selector means "Angular application root element".
+      // We should use [elementRef](https://angular.io/api/core/ElementRef) to get DOM node.
+      // However it's hard to override class's constructor and inject depndency on `ElementRef`.
+      // Is there a better way?
+      const node = document.querySelector('[ng-version]');
+      if (!node) return;
+      imagesLoaded(node, () => {
+        setTimeout(() => emit(EventTypes.COMPONENT_READY, this.__getStoryContext__()), delay);
+      });
     };
 
     clazz.__WRAPPED_WITH_SCREENSHOT__ = true;
