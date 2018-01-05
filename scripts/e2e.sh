2946d9db61b392b691cb517680b9c041ab804ba3
y-kurami <yosuke.kurami@gmail.com>
2018-01-05 18:23:47 +0900

    add e2e script

---
+++ scripts/e2e.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+run() {
+  pushd $1
+  rm -rf __screenshots__
+  npm run screenshot
+  count=$(ls __screenshots__ | wc -w)
+  if [ "$count" -eq 0 ]; then
+    echo "There is no PNG files... 😢"
+    popd
+    exit 1
+  fi
+  echo "✨ screenshot in $1 is ended successfully ✨"
+  popd
+}
+
+run . && run examples/angular-cli
