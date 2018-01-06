be33ce83526f02931d135b57bd870cde53892a08
tsuyoshiwada <wadackel@gmail.com>
2018-01-06 22:12:07 +0900

    Fix withScreenshot type definition

--- src/withScreenshot.ts
+++ src/withScreenshot.ts
@@ -3,15 +3,19 @@ import ReactWithScreenshot from './clients/react/withScreenshot';
 import NgWithScreenshot from './clients/angular/withScreenshot';
 import { PartialScreenshotOptions } from './models/options';
 
+export interface WithScreenshot {
+  <T = Function>(options?: PartialScreenshotOptions): T;
+}
+
 const storybookEnv = getStorybookEnv();
-let withScreenshot: (options?: PartialScreenshotOptions) => Function;
+let withScreenshot: WithScreenshot;
 
 switch (storybookEnv) {
   case 'react':
-    withScreenshot = ReactWithScreenshot;
+    withScreenshot = (ReactWithScreenshot as WithScreenshot);
     break;
   case 'angular':
-    withScreenshot = NgWithScreenshot;
+    withScreenshot = (NgWithScreenshot as WithScreenshot);
     break;
   default:
     throw new Error(`storybook-chrome-screenshot does not support "${storybookEnv}".`);
