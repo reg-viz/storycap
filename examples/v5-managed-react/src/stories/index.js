ed3b6dab11ee55d0fc4ef9f41d8ab5538c35e55d
tsuyoshiwada <wadackel@gmail.com>
2020-01-13 00:38:32 +0900

    fix: Fix a screenshot bug when using hover & focus state at the same story

--- examples/v5-managed-react/src/stories/index.js
+++ examples/v5-managed-react/src/stories/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-
 import { Button, Welcome } from '@storybook/react/demo';
-
+import { isScreenshot } from 'storycap';
 import MyButton from '../MyButton';
 import MyInputText from '../MyInputText';
 import ImageButton from '../ImageButton';
 
-import { isScreenshot } from 'storycap';
-
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-storiesOf('Welcome_override', module)
-.add('to Storybook', () => <Welcome showApp={linkTo('Button')} />, {
+storiesOf('Welcome_override', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />, {
   screenshot: {
     viewport: 'iPhone 6',
     variants: {
@@ -26,7 +21,7 @@ storiesOf('Welcome_override', module)
 });
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello { isScreenshot() ? 'Storycap' : 'Button'}</Button>)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello {isScreenshot() ? 'Storycap' : 'Button'}</Button>)
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
@@ -41,25 +36,27 @@ storiesOf('Button_to_be_skipped', module)
       skip: true,
     },
   })
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>);
 
-storiesOf('MyInputText', module)
-  .add('default', () => <MyInputText />, {
-    screenshot: {
-      focus: 'input[type="text"]',
-    },
-  });
+storiesOf('MyInputText', module).add('default', () => <MyInputText />, {
+  screenshot: {
+    focus: 'input[type="text"]',
+  },
+});
 
-storiesOf('MyButton', module)
-  .add('default', () => <MyButton />, {
-    screenshot: {
-      variants: {
-        hover: {
-          extends: ['LARGE', 'SMALL'],
-          hover: '.my-button',
-        },
+storiesOf('MyButton', module).add('default', () => <MyButton />, {
+  screenshot: {
+    variants: {
+      hovered: {
+        extends: ['LARGE', 'SMALL'],
+        hover: '.my-button',
+      },
+      focused: {
+        extends: ['LARGE', 'SMALL'],
+        focus: '.my-button',
       },
     },
-  });
+  },
+});
 
-storiesOf('ImageButton', module).add('default', () => <ImageButton />)
+storiesOf('ImageButton', module).add('default', () => <ImageButton />);
