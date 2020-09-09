c931925729c15a9bd6f61e8c48915665dd50fae7
Quramy <yosuke.kurami@gmail.com>
2020-09-09 21:31:03 +0900

    add v6 example prj

---
+++ examples/v6-simple/src/Welcome.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { linkTo } from '@storybook/addon-links';
+import { Welcome } from '@storybook/react/demo';
+
+export default {
+  title: 'Welcome',
+};
+
+export const toStorybook = () => <Welcome showApp={linkTo('Button')} />
