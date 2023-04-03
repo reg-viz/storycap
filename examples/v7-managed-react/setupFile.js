05adf1c9f5024bc555652c7a6fdda77e4d39c64d
MH4GF <h.miyagi.cnw@gmail.com>
2023-04-04 00:48:01 +0900

    fix: migrate for testing-react is now deprecated
    ref: https://github.com/storybookjs/testing-react/releases/tag/v2.0.0

--- examples/v7-managed-react/setupFile.js
+++ examples/v7-managed-react/setupFile.js
@@ -1,4 +1,4 @@
-import { setGlobalConfig } from '@storybook/testing-react';
-import * as globalStorybookConfig from './.storybook/preview'; // path of your preview.js file
+import { setProjectAnnotations } from '@storybook/testing-react';
+import * as globalStorybookConfig from './.storybook/preview';
 
-setGlobalConfig(globalStorybookConfig);
+setProjectAnnotations(globalStorybookConfig);
