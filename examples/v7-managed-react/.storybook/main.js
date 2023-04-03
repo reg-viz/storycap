8f6633d849a603bdef8fbe41bf47c09d3f4fdb70
MH4GF <h.miyagi.cnw@gmail.com>
2023-04-03 23:46:11 +0900

    fix: replace @storybook/preset-create-react-app from babel config
    From storybook v7, need to add babel settings in project.
    However, @storybook/preset-create-react-app allows you to use the preset settings.
    ref: https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#babel-mode-v7-exclusively
    
    Also, when I start the storybook dev server, I get the following warning
    
    ```
    WARN Storybook support for Create React App is now a separate preset.
    WARN To use the new preset, install `@storybook/preset-create-react-app` and add it to the list of `addons` in your `.storybook/main.js` config file.
    WARN The built-in preset has been disabled in Storybook 6.0.
    ```
    
    Based on these two points, we decided that adding @storybook/preset-create-react-app would be simpler to build.

--- examples/v7-managed-react/.storybook/main.js
+++ examples/v7-managed-react/.storybook/main.js
@@ -1,6 +1,12 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|mdx)'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', 'storycap', '@storybook/addon-mdx-gfm'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    'storycap',
+    '@storybook/addon-mdx-gfm',
+    '@storybook/preset-create-react-app',
+  ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {}
