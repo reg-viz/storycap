365331cfe5e55c1dae497f8d873c8a935783a609
indigolain <indigo.lain@gmail.com>
2024-03-15 23:22:00 +0900

    Fallback to use corresponding new packages for storybook version 8

--- packages/storycap/src/client/register.ts
+++ packages/storycap/src/client/register.ts
@@ -1,7 +1,16 @@
-import { addons } from '@storybook/addons';
+import type { Addons } from '@storybook/addons';
+
+let addons: Addons | undefined;
+try {
+  addons = require('@storybook/addons').addons;
+} catch {}
+try {
+  addons = require('@storybook/manager-api').addons;
+} catch {}
 
 (window as any).__STORYCAP_MANAGED_MODE_REGISTERED__ = true;
 
-addons.register('storycap', () => {
-  // nothing to do
-});
+addons &&
+  addons.register('storycap', () => {
+    // nothing to do
+  });
